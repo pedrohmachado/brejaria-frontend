43c2ad1d19853abd94c32f6a0fbdff2a7fa4a6cc
Pedro Henrique Machado Silva <pedro.machado@castgroup.com.br>
2019-04-11 15:29:20 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'bootstrap';
+import jQuery from 'jquery';
+
+window.$ = window.jQuery = jQuery;
+
+import 'popper.js';
+import './assets/styles/app.scss';
+
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
